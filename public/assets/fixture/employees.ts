8ff2ad34fe2f6993e5e3a7ec29c772e7495980b9
Diego Anton Cerqueira <Diego.Anton.Cerqueira@es.ey.com>
2025-09-17 18:37:40 +0200

    mock data

---
+++ public/assets/fixture/employees.ts
@@ -0,0 +1,42 @@
+export const employees = [
+    {
+        id: 1,
+        name: "Samuel",
+        description: "Hace modelos de AI rápidos",
+        iconUrl: "assets/img_avatar_chico.png",
+        sex: "masculino",
+        role: "Desarrollador AI",
+        isactive: true,
+        rank: "Senior"
+    },
+    {
+        id: 2,
+        name: "Alejandro",
+        description: "Programa React",
+        iconUrl: "assets/img_avatar_chico.png",
+        sex: "masculino",
+        role: "Desarrollador web",
+        isactive: true,
+        rank: "Junior"
+    },
+    {
+        id: 3,
+        name: "Juan José",
+        description: "Programa Angular",
+        iconUrl: "assets/img_avatar_chico.png",
+        sex: "masculino",
+        role: "Desarrollador de Python",
+        isactive: true,
+        rank: "Junior"
+    },
+    {
+        id: 2,
+        name: "Alejandra",
+        description: "Programa Vue",
+        iconUrl: "assets/img_avatar_chica.jpg",
+        sex: "femenino",
+        role: "Desarrolladora web",
+        isactive: true,
+        rank: "Senior"
+    }
+];
