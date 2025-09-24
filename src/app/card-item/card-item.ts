720ad5605cbec4192459aea33aacf690924cccdd
Diego Anton Cerqueira <Diego.Anton.Cerqueira@es.ey.com>
2025-09-24 18:58:06 +0200

    fourth class

--- src/app/card-item/card-item.ts
+++ src/app/card-item/card-item.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ContentChild, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-card-item',
@@ -6,9 +6,56 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './card-item.html',
   styleUrl: './card-item.css'
 })
-export class CardItem {
+export class CardItem{
   @Input() childEmployee: any;
   @Output() employeeSelected = new EventEmitter<void>();
+  @ContentChild('employeeContent1') employeeContent1?: any;
+
+  constructor() {
+    console.log('CardItem constructor -> Primero en ejecutarse');
+    console.log(this.childEmployee);
+  }
+
+  // Se ejecuta una sola vez al inicio del componente y es un buen lugar para conectar con el API externo
+  ngOnInit() {
+    console.log('CardItem ngOnInit -> Segundo en ejecutarse');
+    console.log(this.childEmployee);
+  }
+ 
+  // Spy -> me permite ver los cambios de cualquier variable de la clase typecript
+  ngOnChanges(changes: any) {
+    console.log('CardItem ngOnChanges -> Cada vez que una propiedad @Input cambia de valor', changes);
+  }
+
+  // Me sirve para hacer una detección de cambios a mano en vez de tener el sistema de gestión de cambios automático de Angular
+  ngDoCheck() {
+    console.log('CardItem ngDoCheck -> Cada vez que Angular hace la detección de cambios');
+  } 
+
+  // Me sirve para acceder al contenido proyectado desde el padre en el component hijo. Es el primer lugar en el que tengo acceso a este contenido
+  ngAfterContentInit() {
+    console.log('CardItem ngAfterContentInit -> Después de que se ha renderizado la vista del componente');
+    console.log(this.employeeContent1);
+  }
+
+  // Sirve para hacer una gestión de cambios a mano del contenido proyectado
+  ngContentChecked() {
+  }
+
+  // Me sirve para acceder al html una vez ha sido renderizado
+  ngAfterViewInit() {
+    console.log('CardItem ngAfterViewInit -> Después de que se ha renderizado la vista del componente');
+  } 
+
+  // Sirve para hacer una gestión de cambios a mano de la vista del componente en el que nos encontramos
+  ngAfterViewChecked() {
+
+  }
+
+  // Se ejecuta una sola vez al final del ciclo de vida del componente y es un buen lugar para desconectar del API externo
+  ngOnDestroy() {
+    console.log('CardItem ngOnDestroy -> Justo antes de que Angular destruya el componente');
+  }
 
   onEmployeeSelected(){
     this.employeeSelected.emit(this.childEmployee);
--- src/app/list-items/list-items.ts
+++ src/app/list-items/list-items.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { employees } from '../../../public/assets/fixture/employees';
+import { DatePipe } from '@angular/common';
+import { CardItem } from '../card-item/card-item';
 
 @Component({
   selector: 'app-list-items',
@@ -8,12 +10,33 @@ import { employees } from '../../../public/assets/fixture/employees';
   styleUrl: './list-items.css'
 })
 export class ListItems {
+  @ViewChild('todayRef') todayRef?: ElementRef;
+  @ViewChildren('component') componentRefs?: QueryList<CardItem>;
+
+
   selectedEmployee: any;
   employees = employees;
   /*firstEmployee = employees[0];
   secondEmployee = employees[1];
   thirdEmployee = employees[2];
   fourthEmployee = employees[3];*/
+
+  today: Date = new Date();
+  tomorrow?: Date;
+  datepipe: DatePipe = new DatePipe('en-US');
+
+  constructor() {
+    console.log('ListItems constructor');
+    console.log(this.todayRef?.nativeElement.innerText);
+  }
+
+  // Pendiente de explicar
+  ngAfterViewInit() {
+    console.log('ListItems ngAfterViewInit -> Después de que se ha renderizado la vista del componente');
+    this.tomorrow = this.datepipe.transform(new Date(this.today.setDate(this.today.getDate() + 1)), 'dd/MM/yyyy') as unknown as Date;
+    console.log('tomorrow', this.tomorrow);
+    console.log('componentRefs', this.componentRefs);
+  } 
 
   onEmployeeSelected(employee: any){
     this.selectedEmployee = employee;
