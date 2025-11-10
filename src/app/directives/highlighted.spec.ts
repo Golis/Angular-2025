58e04378b894e56d9bda372b8ea1028fc6e1a6bb
Diego Anton Cerqueira <Diego.Anton.Cerqueira@es.ey.com>
2025-11-10 20:04:34 +0100

    tests

--- src/app/directives/highlighted.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Highlighted } from './highlighted';
-
-describe('Highlighted', () => {
-  it('should create an instance', () => {
-    const directive = new Highlighted();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/directives/ngx-unless.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NgxUnless } from './ngx-unless';
-
-describe('NgxUnless', () => {
-  it('should create an instance', () => {
-    const directive = new NgxUnless();
-    expect(directive).toBeTruthy();
-  });
-});
